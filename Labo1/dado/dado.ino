0bd30ae53b145b261fd00ad64be5078c7237bcf9
Esteban Vargas <estebanva911@gmail.com>
2016-08-23 23:08:37 -0600

    Fixing blink problems

--- Labo1/dado/dado.ino
+++ Labo1/dado/dado.ino
@@ -9,6 +9,9 @@ int buttonPushCounter = 0; // contador que registra cuantas veces se presiona el
 int buttonState = 0; // variable que almacena el estado del boton
 int lastButtonState = 0; // variable que almacena el estado anterior del boton
 int dado[3]; //vecetor que almacena el valor de cada uno de los 4 leds que componen el dado
+unsigned long preMillis = 0;
+long rebote = 400;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,35 +29,39 @@ void loop() {
 
   //compara el estado del boton y el estado anterior, con base a este calcula el nuevo valor del dado
   if(buttonState != lastButtonState) {
-    dado[0]= random(0,2);
-    dado[1]= random(0,2);
-    dado[2]= random(0,2);
-    dado[3]= random(0,2);
-    if(dado[0] == 1){
-      digitalWrite(9,HIGH);
+    unsigned long currentMillis = millis();
+    if((currentMillis - preMillis) >= rebote){ 
+      preMillis = currentMillis;
+      dado[0]= random(0,256);
+      dado[1]= random(0,256);
+      dado[2]= random(0,256);
+      dado[3]= random(0,256);
+      if(dado[0]%2 == 0){
+        digitalWrite(9,HIGH);
+      }
+      else {
+        digitalWrite(9,LOW);
+      }
+      if(dado[1]%2 == 0){
+        digitalWrite(10,HIGH);
+      }
+      else {
+        digitalWrite(10,LOW);
+      }
+      if(dado[2]%2 == 0){
+        digitalWrite(11,HIGH);
+      }
+      else {
+        digitalWrite(11,LOW);
+      }
+      if(dado[3]%2 == 0){
+        digitalWrite(12,HIGH);
+      }
+      else {
+        digitalWrite(12,LOW);
+      }
+      delay(100);
     }
-    else {
-      digitalWrite(9,LOW);
-    }
-    if(dado[1] == 1){
-      digitalWrite(10,HIGH);
-    }
-    else {
-      digitalWrite(10,LOW);
-    }
-    if(dado[2] == 1){
-      digitalWrite(11,HIGH);
-    }
-    else {
-      digitalWrite(11,LOW);
-    }
-    if(dado[3] == 1){
-      digitalWrite(12,HIGH);
-    }
-    else {
-      digitalWrite(12,LOW);
-    }
-    delay(100);
   }
   lastButtonState = buttonState;
-}
+} 
