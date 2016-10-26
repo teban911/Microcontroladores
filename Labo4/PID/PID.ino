b722e41ff0dddcd2a0d87ca39cd4ec0837d119e2
Esteban Vargas <estebanva911@gmail.com>
2016-10-26 17:04:06 -0600

    Version final del PID

--- Labo4/PID/PID.ino
+++ Labo4/PID/PID.ino
@@ -1,35 +1,45 @@
 #include <PID_v1.h> 
+#include <LiquidCrystal.h>
 
 //Variables
 double Setpoint, Input, Output;
 unsigned long DataRead;
 int Analog_Input;
-String Datos;
+int Dato;
+//char *lcd_msg[20];
+
 
 //Se inicializa el PID: PID(&Input, &Output, &Setpoint, Kp, Ki, Kd, Direction)
-PID myPID(&Input, &Output, &Setpoint,5,0,0, REVERSE);
-
+PID myPID(&Input, &Output, &Setpoint,5,2,0, REVERSE);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
   Serial.begin(9600);
   Input = analogRead(0);
-  Setpoint = 25;
   //Se enciende el PID
   myPID.SetMode(AUTOMATIC);
+  lcd.begin(16, 2);
 }
 
 void loop() {
-  /*if (Serial.available()){
-    Datos = Serial.read(); //Se lee el valor ingresado cada vez que haya una interrupcion por el puerto serial
-    Setpoint = Datos.toFloat();
-    //Setpoint = DataRead;
-  } */
-  Analog_Input = analogRead(0);
+  char buffer[20]= " ";
+  
+  while (Serial.available() > 0) {
+    Dato = Serial.parseInt();
+    Setpoint = (double)Dato;
+  }
+  Analog_Input = analogRead(A0);
   Input = (double)Analog_Input/1024;
   Input = Input*5;
   Input = Input-0.5;
   Input = Input *100;
-  //Serial.println(DataRead);
   myPID.Compute();
-  analogWrite(3,Output);
+  analogWrite(10,Output);
+  lcd.print("Set=");
+  lcd.print(Dato);
+  lcd.print(" ");
+  lcd.print("Tmp=");
+  lcd.print(Input); 
+  delay(1000);
+  lcd.clear();
 }
 
